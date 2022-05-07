d054c3bb58423856b37e085b68f41fa792ae129d
matej352 <61908498+matej352@users.noreply.github.com>
2022-05-07 18:44:23 +0200

    posting image (not done everything yet)

--- src/app/Pages/publish/publish/publish-form/publish-form.component.ts
+++ src/app/Pages/publish/publish/publish-form/publish-form.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { CategoryService } from './../../../../Services/category.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-publish-form',
@@ -17,9 +18,11 @@ export class PublishFormComponent implements OnInit {
 
   public publishForm!: FormGroup;
 
+  fileName: string = '';
+
 
   constructor(private categoryService: CategoryService,
-              public formBuilder: FormBuilder ) { 
+              public formBuilder: FormBuilder, private http: HttpClient ) { 
 
       this.categories$ = categoryService.getAll();
 
@@ -42,6 +45,30 @@ export class PublishFormComponent implements OnInit {
   publish() {
     console.log(this.publishForm.value)
 
+  }
+
+
+  onFileSelected(event: any) {
+    const file:File = event.target.files[0];
+    if (file) {
+
+        this.fileName = file.name;
+        const formData = new FormData();
+        formData.append("thumbnail", file);
+
+        console.log(formData);
+
+        const upload$ = this.http.post("https://localhost:44326/api/image", formData);
+
+        upload$.subscribe();
+    }
+
+  }
+
+
+
+  dohvati() {
+    const upload$ = this.http.get("https://localhost:44326/api/image");
   }
 
 }
