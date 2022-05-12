fd1984fbf427d535bcb98ad903357add45fceb30
matej352 <61908498+matej352@users.noreply.github.com>
2022-05-12 16:32:37 +0200

    fetching images for advertisements

--- src/app/Models/Item.ts
+++ src/app/Models/Item.ts
@@ -4,7 +4,7 @@ export interface Item {
     itemId: number;
     title: string; 
     description: string; 
-    pictureUrl: string; 
+    imageData: string; 
     findingDate: Date; 
     lossDate: Date;
     AdvertisementId: number;
--- src/app/Pages/home/advertisement-list/advertisement-list.component.ts
+++ src/app/Pages/home/advertisement-list/advertisement-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { AdvertisementWithItem } from 'src/app/Models/AdvertisementWithItem';
 import { AdvertisementService } from 'src/app/Services/advertisement.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-advertisement-list',
@@ -15,7 +16,7 @@ export class AdvertisementListComponent implements OnInit {
 
   subscription!: Subscription;
 
-  constructor(private advertisementService: AdvertisementService, private uiService: UiService) {
+  constructor(private advertisementService: AdvertisementService, private uiService: UiService, private sanitizer: DomSanitizer) {
     this.subscription = uiService.onFilterChange().subscribe(filterId => this.applyCategoryFilter(filterId));
   }
 
@@ -26,8 +27,50 @@ export class AdvertisementListComponent implements OnInit {
   }
 
   private applyCategoryFilter(filterId: number) {
-    console.log(filterId);
     this.advertisements$ = this.advertisementService.getAllActiveCategoryFilter(filterId);
   }
+
+  public prepareSource(data: string): SafeResourceUrl {
+    const imagePath = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/jpg;base64,' + data);
+    return imagePath;
+  }
+
+  //get first 29 words of description to show
+  public prepareShortDescription(description: string): string {
+    var shortDescription;
+    if (description.split(" ").length >= 30) {
+      var index = this.getCharCount(description);
+      shortDescription = description.substring(0, description.indexOf(" ", index)) + "...";
+    } else {
+      shortDescription = description;
+    }
+
+    return shortDescription;
+  }
+  
+  
+  getCharCount(description: string): number {
+    var array = description.split(" ");
+    var counter = 0;
+
+    var till30 = 0;
+    for (const word of array) {
+      till30++;
+      if (till30 == 30) {
+        break;
+      }
+      counter++;
+      var chars = word.split('');
+      for (const char of chars) {
+           counter++;
+      }
+         
+    }
+    counter--;
+   
+    return counter;
+  }
+
+  
 
 }
